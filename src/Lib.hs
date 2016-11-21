5c1d4aafbaff5ee8cd9a888b676bafda2076afa3
Simon Hudon <simon.hudon@gmail.com>
2016-11-20 23:20:52 -0500

    added th-printf to stack

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,22 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Control.Monad
+
+import Logic.Expr
+import Logic.Proof.POGenerator
+import Logic.Expr.Parser
+import Logic.Expr.Printable
+
+import Text.LaTeX.FunctionTable
+import Utilities.Syntactic
+import Z3.Z3
+
+parseTable :: ParserSetting
+           -> FunctionTable StringLi 
+           -> Either [Error] (FunctionTable Expr)
+parseTable parser = traverse (fmap getExpr . parse_expr parser)
+
+verifyTable :: TableCells Expr -> POGen ()
+verifyTable t = completeness t >> disjointness t >> wellDefinedness
+
+completeness :: TableCells Expr -> POGen ()
