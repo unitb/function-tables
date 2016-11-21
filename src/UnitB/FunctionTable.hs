00784d7b29a8e4197c01eac4c4caf06fed0afe3b
Simon Hudon <simon.hudon@gmail.com>
2016-11-21 04:21:50 -0500

    added a test case and enabled z3 verification of completeness, disjointness and well-definedness

---
+++ src/UnitB/FunctionTable.hs
@@ -0,0 +1,80 @@
+{-# LANGUAGE QuasiQuotes #-}
+module UnitB.FunctionTable where
+
+import Control.Lens
+import Control.Monad
+import Control.Monad.Trans
+import Control.Monad.Trans.Either
+
+import Data.Bitraversable
+import Data.List as L
+import Data.List.NonEmpty as N
+import Data.Map  as M
+
+import Logic.Expr
+import Logic.Expr.Parser
+import Logic.Proof.POGenerator
+import Logic.Theory
+import Logic.WellDefinedness
+
+import Text.LaTeX.FunctionTable
+import Text.Printf.TH
+import Utilities.Syntactic
+import Z3.Z3
+
+parseTable :: ParserSetting
+           -> FunctionTable LaTeXLI
+           -> Either [Error] (FunctionTable Expr)
+parseTable parser = bitraverse 
+        (fmap getExpr . parse_expr parser0 . toStringLi)
+        (fmap getExpr . parse_expr parser1 . toStringLi)
+    where
+        parser0 = parser & expected_type .~ Just bool
+        parser1 = parser & expected_type .~ Nothing
+
+toStringLi :: LaTeXLI -> StringLi
+toStringLi (LaTeXLI li t) = asStringLi (locToLI li) t
+
+verifyTable :: ParserSetting
+            -> [Theory]
+            -> FunctionTable LaTeXLI
+            -> IO (Either [Error] (Map Label Validity))
+verifyTable p thys t = runEitherT $ do
+    t' <- hoistEither $ parseTable p t
+    lift $ traverseWithKey discharge 
+            . eval_generator 
+            . with (do 
+                _context $ contextOf p
+                forM_ thys $ \thy -> do
+                    let syn = mconcat $ L.map (view syntacticThm) $ all_theories thy
+                    _context $ theory_ctx thy
+                    set_syntactic_props syn
+                    nameless_hyps $ M.elems $ theory_facts thy ) 
+            . functionTablePO $ t'
+
+functionTablePO :: FunctionTable Expr -> POGen ()
+functionTablePO (Table _ t) = functionTablePO' t
+
+functionTablePO' :: TableCells Expr 
+                 -> POGen ()
+functionTablePO' t = do
+    completeness t 
+    disjointness t 
+    wellDefinedness t
+    let subtablePO i (asm,t') = with (prefix (show i) >> nameless_hyps [asm]) $ functionTablePO' t'
+    imapMOf_ isubtables subtablePO t
+
+disjointness :: TableCells Expr -> POGen ()
+disjointness (Cell _) = return ()
+disjointness (Condition _ xs) = sequence_ $ do
+        (i,(y,_),ys) <- L.zip3 [0..] (N.toList xs) $ N.tail $ N.tails (N.zip (0:|[1..]) xs)
+        (j,(z,_))      <- ys
+        [emit_goal [label $ [s|disjointness-%d-%d|] i j] $ znot y `zor` znot z]
+
+completeness :: TableCells Expr -> POGen ()
+completeness (Cell _) = return ()
+completeness (Condition _ xs) = emit_goal [label "completeness"] $ zsome $ fst <$> xs
+
+wellDefinedness :: TableCells Expr -> POGen ()
+wellDefinedness (Cell p) = emit_goal [label "WD"] $ well_definedness p
+wellDefinedness (Condition _ xs) = imapM_ (\i -> emit_goal [label $ [s|WD/%d|] i].well_definedness.fst) xs
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,66 @@
+
+{-# LANGUAGE QuasiQuotes,OverloadedStrings #-}
+module Main where
+
+import Control.Concurrent
+import Control.Lens
+import Data.Bitraversable
+import Data.Map
+
+import Logic.QuasiQuote
+import Logic.Theories.Arithmetic
+import Logic.Theories.SetTheory
+import Logic.Theories.FunctionTheory
+import Logic.Theory
+
+import System.Exit
+import System.Process
+
+import Text.LaTeX
+import Text.LaTeX.Base.Class
+import Text.LaTeX.FunctionTable
+
+import UnitB.FunctionTable
+
+import Z3.Z3
+
+doc :: Render a => a -> LaTeX
+doc t = 
+    documentclass [] article
+ <> usepackage [] "multirow"
+ -- <> title "A short message"
+ -- <> author "John Short"
+ -- <> author "John Short"
+ <> comm1 "newcommand" (raw "\\dom") <> braces (textsf "dom")
+ <> document (rendertex t)
+
+ft :: FunctionTable LaTeXLI
+ft = makeTable "x" $ do
+        cell "y < 3"                    "x + 1"
+        branch "y = 3" $ do 
+            cell "x \\in \\dom.f"       "f.x"
+            cell "\\neg x \\in \\dom.f" "x"
+        cell "y > 3"                    "x - 1"
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do
+        setNumCapabilities 8
+            --
+            -- Verification
+            --
+        let ts = [ arithmetic
+                 , function_theory
+                 , set_theory
+                 , basic_theory]
+            parser = ctxWith ts 
+                (do [var| x,y : \Int |]
+                    [var| f : \Int \pfun \Int |]) id
+        r <- verifyTable parser ts ft
+        if has (_Right.filtered ((7 ==) . size).filtered (all (Valid ==))) r then exitSuccess
+                   else exitFailure
+            -- 
+            -- Rendering
+            -- 
+        -- renderFile "table.tex" (doc ft)
+        -- rawSystem "pdflatex" ["table.tex"]
+        return ()
